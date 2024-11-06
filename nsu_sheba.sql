486e56687200fe51dfef6ee6c5ed178dbdf18a18
Md. Hasan Emam <119060696+mdhasanemam@users.noreply.github.com>
2024-11-06 22:03:10 +0600

    Add files via upload

---
+++ nsu_sheba.sql
@@ -0,0 +1,267 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 06, 2024 at 04:56 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nsu_sheba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agendas`
+--
+
+CREATE TABLE `agendas` (
+  `agenda_id` int(11) NOT NULL,
+  `student_id` varchar(255) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `expire_at` datetime NOT NULL,
+  `min_votes` int(11) NOT NULL,
+  `vote_for` int(11) DEFAULT 0,
+  `vote_against` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `agendas`
+--
+
+INSERT INTO `agendas` (`agenda_id`, `student_id`, `title`, `description`, `created_at`, `expire_at`, `min_votes`, `vote_for`, `vote_against`) VALUES
+(23, '2212478042', 'Boycott china ', 'they..', '2024-10-22 15:31:09', '2024-11-05 11:17:02', 0, 0, 0),
+(24, '2212478042', 'Boycot israel product', 'tehy..', '2024-10-22 16:56:07', '2024-10-22 16:57:00', 0, 7, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blood_requests`
+--
+
+CREATE TABLE `blood_requests` (
+  `request_id` int(11) NOT NULL,
+  `requester_student_id` varchar(255) DEFAULT NULL,
+  `patient_name` varchar(255) DEFAULT NULL,
+  `patient_age` int(11) DEFAULT NULL,
+  `requested_blood_group` varchar(255) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `date_of_need` date DEFAULT NULL,
+  `bags_needed` int(11) DEFAULT NULL,
+  `additional_notes` text DEFAULT NULL,
+  `bags_fulfilled` int(11) DEFAULT 0,
+  `status` enum('pending','fulfilled') DEFAULT 'pending',
+  `last_accepted_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `blood_requests`
+--
+
+INSERT INTO `blood_requests` (`request_id`, `requester_student_id`, `patient_name`, `patient_age`, `requested_blood_group`, `address`, `date_of_need`, `bags_needed`, `additional_notes`, `bags_fulfilled`, `status`, `last_accepted_date`) VALUES
+(46, '2212478042', 'Sadman Sameer', 45, 'O+', 'sss', '2024-11-07', 2, '', 1, 'pending', '2024-11-06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donors`
+--
+
+CREATE TABLE `donors` (
+  `donor_id` int(11) NOT NULL,
+  `student_id` varchar(255) DEFAULT NULL,
+  `donor_blood_group` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `phone_number` varchar(15) DEFAULT NULL,
+  `health_condition` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `donors`
+--
+
+INSERT INTO `donors` (`donor_id`, `student_id`, `donor_blood_group`, `age`, `phone_number`, `health_condition`) VALUES
+(22, '2211255042', 'O+', 25, '0179447902', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp_users`
+--
+
+CREATE TABLE `temp_users` (
+  `student_id` varchar(255) DEFAULT NULL,
+  `student_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `otp` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `student_id` varchar(255) DEFAULT NULL,
+  `student_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `otp` int(6) DEFAULT NULL,
+  `otp_expires` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`student_id`, `student_name`, `email`, `password`, `gender`, `otp`, `otp_expires`) VALUES
+('2212478042', 'Md. Hasan Emam', 'hasan.emam@northsouth.edu', '$2y$10$lwoJPUp8bIy/r9aOAzpPQOP.6Q9apThajORSuVRH1u4gupTAsQnSe', 'Male', 809333, '2024-10-10 07:08:49'),
+('2211255042', 'Minhaz', 'minhazur.rahman12@northsouth.edu', '$2y$10$FN/yACCkFafl14mEGeHtpeccJ.kLMNXzNdldGgf7sgMCxl4axYBVq', 'Male', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE `votes` (
+  `vote_id` int(11) NOT NULL,
+  `student_id` varchar(255) DEFAULT NULL,
+  `agenda_id` int(11) DEFAULT NULL,
+  `vote_type` enum('for','against') NOT NULL,
+  `voted_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`vote_id`, `student_id`, `agenda_id`, `vote_type`, `voted_at`) VALUES
+(3, '2212478042', 24, 'for', '2024-10-22 16:56:14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agendas`
+--
+ALTER TABLE `agendas`
+  ADD PRIMARY KEY (`agenda_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `blood_requests`
+--
+ALTER TABLE `blood_requests`
+  ADD PRIMARY KEY (`request_id`),
+  ADD KEY `requester_student_id` (`requester_student_id`);
+
+--
+-- Indexes for table `donors`
+--
+ALTER TABLE `donors`
+  ADD PRIMARY KEY (`donor_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `temp_users`
+--
+ALTER TABLE `temp_users`
+  ADD UNIQUE KEY `student_id` (`student_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `student_id` (`student_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`vote_id`),
+  ADD UNIQUE KEY `student_id` (`student_id`,`agenda_id`),
+  ADD KEY `agenda_id` (`agenda_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `agendas`
+--
+ALTER TABLE `agendas`
+  MODIFY `agenda_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `blood_requests`
+--
+ALTER TABLE `blood_requests`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `donors`
+--
+ALTER TABLE `donors`
+  MODIFY `donor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `votes`
+--
+ALTER TABLE `votes`
+  MODIFY `vote_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `agendas`
+--
+ALTER TABLE `agendas`
+  ADD CONSTRAINT `agendas_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`student_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `blood_requests`
+--
+ALTER TABLE `blood_requests`
+  ADD CONSTRAINT `blood_requests_ibfk_1` FOREIGN KEY (`requester_student_id`) REFERENCES `users` (`student_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `donors`
+--
+ALTER TABLE `donors`
+  ADD CONSTRAINT `donors_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`student_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `votes`
+--
+ALTER TABLE `votes`
+  ADD CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`student_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`agenda_id`) REFERENCES `agendas` (`agenda_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
